9ea604bdf84a6fbfabd122b7b5ae00ebbe00022f
Andrevv Avtomonov <web@hu2.ru>
2024-05-01 09:37:19 +0300

    1

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ const bot = new TelegramApi(token, {polling: true})
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, msg);
 });
